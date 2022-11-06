0172845f36dd8e4e3ac4d9767caa827854051b4f
Alex <sashamarx67@gmail.com>
2022-11-06 16:41:56 +0300

    добавил рассылку по времени, соеденил с джанго

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from aiogram import executor
+import asyncio
+import os
 
-from loader import dp
-import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
+import django
 
 
 async def on_startup(dispatcher):
@@ -14,6 +12,36 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+async def scheduler():
+    aioschedule.every().day.at('10:00').do(notify)
+    aioschedule.every().day.at('15:00').do(notify)
+    aioschedule.every().day.at('18:00').do(notify)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
 
+
+def setup_django():
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        'admin_panel.admin_panel.settings'
+    )
+    os.environ.update({"DJANGO_ALLOW_ASYNC_UNSAFE": "true"})
+    django.setup()
+
+
+if __name__ == '__main__':
+    setup_django()
+    import middlewares, filters, handlers
+    from utils import on_startup_notify
+    from utils.set_bot_commands import set_default_commands
+    from utils.mailing import notify
+    import aioschedule
+    from aiogram import executor
+
+    from loader import dp
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(scheduler())
+
+    executor.start_polling(dp, on_startup=on_startup)
